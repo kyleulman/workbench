c73405b3f76a874e8ee1b2932b50f6a25f432a6a
kyleulman <kyle@ulman.digital>
2023-04-25 17:18:42 -0400

    feat: add Quote component

--- src/routes/sandbox/+page.svelte
+++ src/routes/sandbox/+page.svelte
@@ -27,6 +27,14 @@
 	/>
 </header>
 <section>
+	<Quote content={{
+		detail: "Did you hear that? They've shut down the main reactor. We'll be destroyed for sure. This is madness! We're doomed! There'll be no escape for the Princess this time. What's that? Artoo! Artoo-Detoo, where are you? At last! Where have you been? They're heading in this direction."
+		,name: "George Lucas",
+		href: "#star-wars",
+		label: "Star Wars: Episode IV - A New Hope"
+	}}/>
+</section>
+<section>
 	<Preload content={{
 		src: "https://picsum.photos/768/384", 
 		alt: "Placeholder.", 
